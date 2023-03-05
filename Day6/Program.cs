23938d2986f1b9671938c2086c1bc09082121f0c
Mrazma <mraz.matej05@gmail.com>
2023-03-05 10:36:24 +0100

    Day 6 - Complete and working

---
+++ Day6/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Day6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = File.ReadAllText("../../../input.txt");
+
+
+            for (int i = 4; i < line.Length; i++) // Part I
+            {
+                string current4 = line.Substring(i - 4, 4);
+
+                if (current4.Length == current4.Distinct().Count())
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+
+            for (int i = 14; i < line.Length; i++) // Part II
+            {
+                string current14 = line.Substring(i-14, 14);
+
+                if (current14.Length == current14.Distinct().Count())
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+
+        }
+    }
+}
